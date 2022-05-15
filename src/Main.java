879abacfca05de0e44ae6dc8c780841fab10f2a5
HotakMuhib <123457don@gmail.com>
2022-05-15 12:06:25 -0400

    red audi car with basic feature

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         //write your code here
-        BasicFeature car = new BasicFeature();
-        
+        BasicFeature Audi = new BasicFeature();
+        Audi.color = "red";
+        Audi.doors = "2";
+        Audi.engineCapacity="v12";
+        Audi.headlight = "blue";
+        Audi.regNo = "1234";
+
     }
 }
 
